ac88a64bacdd028a87584ad1e1e0f6880529fe2e
Martí <martipuig@protonmail.com>
2020-04-06 21:55:07 +0100

    Defines grau d'assoliment of each test (E)

--- Index.py
+++ Index.py
@@ -15,6 +15,26 @@ def get_filters():
 
     print('-'*40)
     return classe.lower()
+
+def assoliments(df):
+    # if a mark is not there, fills no marks with a 0
+    df['E1-C']=df['E1-C'].fillna(0)
+    df['E2-C']=df['E2-C'].fillna(0)
+    df['E3-C']=df['E3-C'].fillna(0)
+    df['E4-C']=df['E4-C'].fillna(0)
+    # make a mean
+    df['Prob']=(df['E1-P']+df['E2-P']+df['E3-P']+df['E4-P'])/4+1
+    df['Raon']=(df['E1-R']+df['E2-R']+df['E3-R']+df['E4-R'])/4+1
+    df['Conn']=(df['E1-C']+df['E2-C']+df['E3-C']+df['E4-C'])/4+1
+    df['Repre']=(df['E1-I']+df['E2-I']+df['E3-I']+df['E4-I'])/4+1
+    #put it in NA, AS, AN and AE
+    df['Prob1'] = df['Prob'].apply(lambda x: 'AE' if x >= 3.5 else 'AN' if x >=2.5 else 'AS' if x>=1.75 else 'NA' )
+    df['Raon1'] = df['Raon'].apply(lambda x: 'AE' if x >= 3.5 else 'AN' if x >=2.5 else 'AS' if x>=1.75 else 'NA' )
+    df['Conn1'] = df['Conn'].apply(lambda x: 'AE' if x >= 3.5 else 'AN' if x >=2.5 else 'AS' if x>=1.75 else 'NA' )
+    df['Repre1'] = df['Repre'].apply(lambda x: 'AE' if x >= 3.5 else 'AN' if x >=2.5 else 'AS' if x>=1.75 else 'NA' )
+    return
+
+
 def main():
     while True:
         classe = get_filters()    Temporalment inactiu
